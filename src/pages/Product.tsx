31a4bb5e2a931d55f6a582bcdb7d3b041d600480
Rafael Gonzaga <rafael_hgcarlos@hotmail.com>
2025-05-19 20:02:49 -0300

    feat: add product page with welcome message

---
+++ src/pages/Product.tsx
@@ -0,0 +1,8 @@
+export default function Product() {
+    return (
+        <div className="p-2">
+            <h2 className="text-2xl font-bold mb-4">Product</h2>
+            <p>Bem-vindo ao painel principal! Aqui você pode visualizar um resumo das informações do sistema.</p>
+        </div>
+    );
+}
